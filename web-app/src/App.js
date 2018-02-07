97f510eb504576d1fda67aa65cb16fb4332b5b30
Softwerk\Kushtrim <kushtrim.abdiu@softwerk.se>
2018-02-07 19:41:51 +0100

    Initiating the web app

---
+++ web-app/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+export default class App extends React.Component {
+
+    render() {
+        return(<center><h1>HELLO WORLD </h1></center>);
+    }
+}
